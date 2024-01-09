b3e9d8e8dbfd14d5a87966cbc4f24b4a35c3868d
alumno <alumno@1DAW-27>
2024-01-09 13:08:24 +0100

    Paso1 Alumno1(cristian)

--- src/Proyecto/AreaTriangulo.java
+++ src/Proyecto/AreaTriangulo.java
@@ -8,12 +8,15 @@ static double l1=0;
 static double l2=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		calcularArea();
 	}
-	public static double calcularArea(int lado1, int lado2) {
-		System.out.println("Introduce lado1");
+	public static void calcularArea() {
+		System.out.println("Introduce base del triangulo");
 		l1=in.nextInt();
-		System.out.println("Introduce lado2");
+		System.out.println("Introduce altura del triangulo");
 		l2=in.nextDouble();
+		System.out.println("El area del triangulo es "+ l1 + " x "+ l2 + " = "+ ((l1*l2)/2));
+		
 	}
 
 }
