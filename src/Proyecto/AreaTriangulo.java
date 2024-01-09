39ece60b3aeb1b64e9040476e419d39cc5f9bde9
alumno <alumno@1DAW-27>
2024-01-09 12:49:03 +0100

    Paso1 Alumno1(cristian)

--- src/Proyecto/AreaTriangulo.java
+++ src/Proyecto/AreaTriangulo.java
@@ -1,10 +1,19 @@
 package Proyecto;
 
-public class AreaTriangulo {
+import java.util.Scanner;
 
+public class AreaTriangulo {
+static Scanner in=new Scanner(System.in);
+static double l1=0;
+static double l2=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola");
+	}
+	public static double calcularArea(int lado1, int lado2) {
+		System.out.println("Introduce lado1");
+		l1=in.nextInt();
+		System.out.println("Introduce lado2");
+		l2=in.nextDouble();
 	}
 
 }
